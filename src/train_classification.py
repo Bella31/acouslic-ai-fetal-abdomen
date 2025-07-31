3de49366b9acacc376b54a807250bb347dc2f713
Merry Shalabi <Merryshalabi59@gmail.com>
2025-07-31 19:06:16 +0300

    Update train_classification.py

--- src/train_classification.py
+++ src/train_classification.py
@@ -2,6 +2,8 @@
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 from torchvision import transforms
+from src.model_resnet import get_finetune_resnet_model
+from src.utils import FocalLoss
 import os
 
 def get_dataloaders(base_path, batch_size):
@@ -37,3 +39,22 @@ def get_dataloaders(base_path, batch_size):
     )
 
     return train_loader, val_loader, test_loader
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model = get_finetune_resnet_model(num_classes=3)
+criterion = FocalLoss()
+optimizer = torch.optim.AdamW(
+    filter(lambda p: p.requires_grad, model.parameters()),
+    lr=3e-4,
+    weight_decay=1e-4
+)
+
+def score_frames(model, frames_tensor):
+    model.eval()
+    with torch.no_grad():
+        frames_tensor = frames_tensor.to(device)
+        outputs = model(frames_tensor)
+        probs = torch.softmax(outputs, dim=1).cpu().numpy()
+    return probs
