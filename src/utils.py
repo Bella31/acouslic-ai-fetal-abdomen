62e3435f7a08c9eebe11314da47bd66a25687b7e
Merry Shalabi <Merryshalabi59@gmail.com>
2025-07-31 19:03:14 +0300

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2.0):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+
+    def forward(self, inputs, targets):
+        ce_loss = nn.CrossEntropyLoss()(inputs, targets)
+        pt = torch.exp(-ce_loss)
+        focal_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
+        return focal_loss.mean()
